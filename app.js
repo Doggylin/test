a26d48c0fa9c757ca1bd8500940d820e9cfd91d1
Dogd <lcl1994@outlook.com>
2018-12-18 18:22:03 +0800

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+const koa = require('koa')
+const fs = require('fs')
+const static = require('koa-static')
+const path = require('path')
+const app = new koa()
+
+app.use(static(
+    path.join(__dirname,'./web')
+))
+app.use(async (ctx,next) => {
+    await next();
+    ctx.response.type = 'text/html';
+    let str = fs.createReadStream('web/categories.html')
+    ctx.response.body = str
+})
+app.listen(3000)
